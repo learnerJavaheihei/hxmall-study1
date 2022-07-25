fb3157be74dcefd320da1ffd5c364d6719deff6e
gj <953478221@qq.com>
2022-07-25 19:17:46 +0800

    提交3

---
+++ fengyuwisdom/src/main/java/com/hxzy/entity/Admin.java
@@ -0,0 +1,53 @@
+package com.hxzy.entity;
+
+public class Admin {
+    private Integer id;
+
+    private String name;
+
+    private String no;
+
+    private String password;
+
+    private Integer flag;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no == null ? null : no.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Integer getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Integer flag) {
+        this.flag = flag;
+    }
+}
